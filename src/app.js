daa2dcef5350c5992a3cfa90ff39eca1e6e665fe
brayansbaezm <brayansbaezm@INTER-PF3G6L26>
2025-12-03 12:03:52 -0500

    update CORS

--- src/app.js
+++ src/app.js
@@ -4,6 +4,18 @@ const app = express();
 
 app.use(express.json());
 
+// CORS middleware: permitir acceso desde cualquier origen
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  // responder a preflight
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  next();
+});
+
 app.use('/api', routes);
 
 app.get('/', (req, res) => {
